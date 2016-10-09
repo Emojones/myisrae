38d4592b0f085d1d47603e2c22a0fb4068eacf77
Michael <emonomics@gmail.com>
2016-10-09 14:21:30 -0400

    installed mongoose, angular, body-parser. copied over code from mean-todo to get me started

---
+++ app/scripts/services/data.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var angular = require('angular');
+
+angular.module('todoListApp')
+.service('dataService', function($http, $q) {
+  this.getTodos = function(cb) {
+    $http.get('/api/todos').then(cb);
+  };
+  
+  this.delete = function(todo) {
+    console.log("I deleted the " + todo.name + " todo!");
+  };
+  
+  this.saveTodos = function(todos) {
+    var queue = [];
+    todos.forEach(function(todo){
+      var request;
+      if(!todo._id) {
+        request = $http.post('/api/todos', todo)
+      } else {
+         request = $http.put('/api/todos/' + todo._id , todo).then(function(result){
+           todo = result.data.todo;
+           return todo;
+         })
+      };
+      queue.push(request);
+    });
+    return $q.all(queue).then(function(results){
+      console.log("I saved "+ todos.length + " todos!");
+    })
+    console.log("I saved " + todos.length + " todos!");
+  }
+});
--- src/app.js
+++ src/app.js
@@ -1,20 +1,23 @@
 'use strict';
 
 var express = require('express');
-//var parser = require('body-parser');
+var app = express();
+var parser = require('body-parser');
 //var router = require('./public');
 
-var app = express();
+
 
 //require('./database');
 //require ('./seed');
 
-//app.use('/', express.static('public'));
+app.use('/', express.static('public'));
 //app.use(parser.json());
 
+
 //app.use('/public', router);
-app.use('/', express.static('public'))
 
 app.listen(3000, function(){
   console.log("The server is running on port 3000");
 });
+
+exports = module.exports = app;
