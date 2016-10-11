92e945b2c54a1f5829f5dfa7811f2173059a5a93
Michael Christy <emonomics@gmail.com>
2016-10-11 19:51:26 -0400

    deleted extrenous files and condesend structure

---
+++ public/app/scripts/directives/page.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var angular = require('angular');
+var app = angular();     
+app.directive('navbar', function(){
+  return {
+    restrict : 'E',
+    templateUrl: 'public/html/navbar.html'
+  };
+});
--- src/app.js
+++ src/app.js
@@ -4,21 +4,18 @@
 var express = require('express');
 var app = express();
 var parser = require('body-parser');
-//var router = require('./public');
+var mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/blog-posts')
 
-
-require('./database');
-//require ('./seed');
-
-app.use('/', express.static('public'));
-//app.use(parser.json());
-
-
-//app.use('/public', router);
-
-app.listen(3000, function(){
-  console.log("The server is running on port 3000");
+// define model =================
+var blogMongo = mongoose.model('blogPost', {
+    name: String,
+    body: String,
 });
 
-exports = module.exports = app;
+app.use('/', express.static('public'));
+
+app.listen(3000, function () {
+    console.log("The server is running on port 3000");
+});
