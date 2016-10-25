87cf84eb4890f2113f735acc9ef401afafd5aa25
Michael Christy <emonomics@gmail.com>
2016-10-25 18:35:50 -0400

    beautify files

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -1,26 +1,28 @@
 'use strict'
-angular.module('app', ['ngRoute']).config(['$routeProvider', function($routeProvider){
-$routeProvider
-  .when('/', {
-    templateUrl: '../html/home.html'
-  })
+angular.module('app', ['ngRoute']).config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'html/home.html'
+        })
 
-  .when('/about', {
-    templateUrl: '/html/about.html'
-  })
+    .when('/about', {
+        templateUrl: 'html/about.html'
+    })
 
-  .when('/media', {
-    templateUrl: '/../html/media.html'
-  })
+    .when('/media', {
+        templateUrl: 'html/media.html'
+    })
 
-  .when('/blog', {
-    templateUrl: '/../html/blog.html'
-  })
+    .when('/blog', {
+        templateUrl: 'html/blog.html'
+    })
 
-  .when('/tours', {
-    templateUrl: '/../html/tours.html'
-  })
+    .when('/tours', {
+        templateUrl: 'html/tours.html'
+    })
 
-  .otherwise ({redirectTO: '/'});
+    .otherwise({
+        redirectTO: '/'
+    });
 
 }]);
