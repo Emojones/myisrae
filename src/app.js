2bc7bae85900fc2efee1957b4d74eb83ba18d95a
Michael <emonomics@gmail.com>
2016-11-02 20:18:37 -0400

    seed data wont recreate each time the server is restarted now

--- src/app.js
+++ src/app.js
@@ -5,17 +5,19 @@ var express = require('express');
 var app = express();
 var parser = require('body-parser');
 var mongoose = require('mongoose');
-//var blogMock = require('../mock/blogMock.json');
 var routes = require('../app/routes');
+
 //import config files
 var db = require('../config/db');
-//require ('../mock/seed');
+
+//seed the database
+require ('../mock/seed');
 
 //serve these static files
 app.use(parser.json());
 app.use(express.static(__dirname + '/../public'));
 
-//connect to mongo
+//connect to mongodb
 mongoose.connect(db.url, function(err){
   if (err) {
     console.log("Unable to connect to database")
@@ -23,7 +25,6 @@ mongoose.connect(db.url, function(err){
 });
 
 app.get('/api/blog', routes);
-
 
 //start the server and print out to the console
 app.listen(3000, function () {
