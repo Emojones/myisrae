77ccf1e846250bead80fc6d45bf83df58d2facb9
Michael <emonomics@gmail.com>
2016-11-07 18:56:29 -0500

    wiring post route start

--- public/js/controllers/xeditable.js
+++ public/js/controllers/xeditable.js
@@ -56,4 +56,13 @@ angular.module('app')
     $scope.body = {
         body: ""
     };
-});
+    $scope.newBlog = function() {
+      $http.post('/api/blog/')
+            .success(function(data) {
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error' + data);
+            });
+    };
+  });
